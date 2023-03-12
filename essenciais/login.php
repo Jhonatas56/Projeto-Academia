f5c682638598d0fdbb87ed73fbb96bf2847931bf
Jhonatas56 <118845977+Jhonatas56@users.noreply.github.com>
2023-03-12 12:26:53 -0300

    Arquivos do Projeto

---
+++ essenciais/login.php
@@ -0,0 +1,32 @@
+<?php
+  include 'banco.php';
+
+  $email = $_POST['email'];
+  $senha = $_POST['senha'];
+
+  $sql = "select * from tbusu where email='$email' and senha='$senha'";
+
+  $consulta = $conexao -> query($sql);
+
+  if($consulta){
+    if($consulta -> num_rows > 0){
+      session_start();
+      $_SESSION['login'] = 'ok';
+      $linha = $consulta -> fetch_array(MYSQLI_ASSOC);
+      
+      //Testa o cargo para ver se é um gerente, educador físico ou aluno
+      if($linha['cargo'] = 'G'){
+        header('Location: ../admin.php');
+      }
+      else if($linha['cargo'] = 'E'){
+        header('Location: ../educador.php');
+      } else {
+        header('Location: ../aluno.php');
+      }
+
+    } 
+    else {
+      header('Location: ../index.php?login=erro'); 
+    }
+  }
+?>
--- index.php
+++ index.php
@@ -26,14 +26,14 @@
             <div class="card col-lg-4 mx-auto">
               <div class="card-body px-5 py-5">
                 <h3 class="card-title text-left mb-3">Projeto Academia</h3>
-                <form>
+                <form method="post" action="essenciais/login.php">
                   <div class="form-group">
                     <label>Email</label>
-                    <input type="text" class="form-control p_input">
+                    <input type="email" class="form-control p_input" name="email" id="email">
                   </div>
                   <div class="form-group">
                     <label>Senha</label>
-                    <input type="text" class="form-control p_input">
+                    <input type="password" class="form-control p_input" name="senha" id="senha">
                   </div>
                   <div class="text-center">
                     <button type="submit" class="btn btn-primary btn-block enter-btn">Login</button>
