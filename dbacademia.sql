e2ce0137103a768be8367635c7c73806d71b94ed
Jhonatas56 <118845977+Jhonatas56@users.noreply.github.com>
2023-03-25 17:22:40 -0300

    Update dbacademia.sql

--- dbacademia.sql
+++ dbacademia.sql
@@ -1,28 +1,35 @@
--- --------------------------------------------------------
--- Servidor:                     127.0.0.1
--- Versão do servidor:           10.4.24-MariaDB - mariadb.org binary distribution
--- OS do Servidor:               Win64
--- HeidiSQL Versão:              12.1.0.6537
--- --------------------------------------------------------
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 12-Mar-2023 às 17:05
+-- Versão do servidor: 10.4.25-MariaDB
+-- versão do PHP: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Banco de dados: `dbacademia`
+--
 
--- Copiando estrutura do banco de dados para dbacademia
-CREATE DATABASE IF NOT EXISTS `dbacademia` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
-USE `dbacademia`;
+-- --------------------------------------------------------
 
--- Copiando estrutura para tabela dbacademia.tbaluno
+--
+-- Estrutura da tabela `tbaluno`
+--
+
 CREATE TABLE IF NOT EXISTS `tbaluno` (
-  `idaluno` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `idusu` int(11) unsigned DEFAULT NULL,
+  `idaluno` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `idusu` int(11) UNSIGNED DEFAULT NULL,
   `nome` varchar(100) NOT NULL,
   `sexo` char(1) NOT NULL,
   `data_nasc` date NOT NULL,
@@ -31,18 +38,20 @@ CREATE TABLE IF NOT EXISTS `tbaluno` (
   `rua` varchar(100) NOT NULL,
   `numero_casa` int(5) NOT NULL,
   PRIMARY KEY (`idaluno`),
-  KEY `fk_usu` (`idusu`),
-  CONSTRAINT `fk_usu` FOREIGN KEY (`idusu`) REFERENCES `tbusu` (`idusu`) ON DELETE CASCADE ON UPDATE CASCADE
+  KEY `fk_usu` (`idusu`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- Copiando dados para a tabela dbacademia.tbaluno: ~0 rows (aproximadamente)
+-- --------------------------------------------------------
 
--- Copiando estrutura para tabela dbacademia.tbavaliacao
+--
+-- Estrutura da tabela `tbavaliacao`
+--
+
 CREATE TABLE IF NOT EXISTS `tbavaliacao` (
-  `idava` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `idaluno` int(11) unsigned DEFAULT NULL,
-  `ideducador` int(11) unsigned DEFAULT NULL,
-  `idgrupo` int(11) unsigned DEFAULT NULL,
+  `idava` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `idaluno` int(11) UNSIGNED DEFAULT NULL,
+  `ideducador` int(11) UNSIGNED DEFAULT NULL,
+  `idgrupo` int(11) UNSIGNED DEFAULT NULL,
   `datava` date NOT NULL,
   `horaava` time NOT NULL,
   `med_braco` int(3) NOT NULL,
@@ -53,96 +62,142 @@ CREATE TABLE IF NOT EXISTS `tbavaliacao` (
   PRIMARY KEY (`idava`),
   KEY `fk_aluno` (`idaluno`),
   KEY `fk_educador` (`ideducador`),
-  KEY `fk_grupo` (`idgrupo`),
-  CONSTRAINT `fk_aluno` FOREIGN KEY (`idaluno`) REFERENCES `tbaluno` (`idaluno`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `fk_educador` FOREIGN KEY (`ideducador`) REFERENCES `tbeducador` (`ideducador`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `fk_grupo` FOREIGN KEY (`idgrupo`) REFERENCES `tbgrupo` (`idgrupo`) ON DELETE CASCADE ON UPDATE CASCADE
+  KEY `fk_grupo` (`idgrupo`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- Copiando dados para a tabela dbacademia.tbavaliacao: ~0 rows (aproximadamente)
+-- --------------------------------------------------------
 
--- Copiando estrutura para tabela dbacademia.tbeducador
+--
+-- Estrutura da tabela `tbeducador`
+--
+
 CREATE TABLE IF NOT EXISTS `tbeducador` (
-  `ideducador` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `idusu` int(11) unsigned DEFAULT NULL,
+  `ideducador` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `idusu` int(11) UNSIGNED DEFAULT NULL,
   `nome` varchar(50) NOT NULL,
   `sexo` char(1) NOT NULL,
   `data_nasc` date NOT NULL,
   `cpf` varchar(14) NOT NULL,
   PRIMARY KEY (`ideducador`),
-  KEY `fk_usu2` (`idusu`),
-  CONSTRAINT `fk_usu2` FOREIGN KEY (`idusu`) REFERENCES `tbusu` (`idusu`) ON DELETE CASCADE ON UPDATE CASCADE
+  KEY `fk_usu2` (`idusu`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- Copiando dados para a tabela dbacademia.tbeducador: ~0 rows (aproximadamente)
+-- --------------------------------------------------------
 
--- Copiando estrutura para tabela dbacademia.tbexercicio
+--
+-- Estrutura da tabela `tbexercicio`
+--
+
 CREATE TABLE IF NOT EXISTS `tbexercicio` (
-  `idexercicio` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `idgrupo` int(11) unsigned DEFAULT NULL,
+  `idexercicio` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `idgrupo` int(11) UNSIGNED DEFAULT NULL,
   `nome` varchar(100) NOT NULL,
   PRIMARY KEY (`idexercicio`),
-  KEY `fk_grupo2` (`idgrupo`),
-  CONSTRAINT `fk_grupo2` FOREIGN KEY (`idgrupo`) REFERENCES `tbgrupo` (`idgrupo`) ON DELETE CASCADE ON UPDATE CASCADE
+  KEY `fk_grupo2` (`idgrupo`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- Copiando dados para a tabela dbacademia.tbexercicio: ~0 rows (aproximadamente)
+-- --------------------------------------------------------
 
--- Copiando estrutura para tabela dbacademia.tbficha
+--
+-- Estrutura da tabela `tbficha`
+--
+
 CREATE TABLE IF NOT EXISTS `tbficha` (
-  `idficha` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `idava` int(11) unsigned DEFAULT NULL,
+  `idficha` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `idava` int(11) UNSIGNED DEFAULT NULL,
   `datficha` date NOT NULL,
   PRIMARY KEY (`idficha`),
-  KEY `fk_ava` (`idava`),
-  CONSTRAINT `fk_ava` FOREIGN KEY (`idava`) REFERENCES `tbavaliacao` (`idava`) ON DELETE CASCADE ON UPDATE CASCADE
+  KEY `fk_ava` (`idava`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- Copiando dados para a tabela dbacademia.tbficha: ~0 rows (aproximadamente)
+-- --------------------------------------------------------
 
--- Copiando estrutura para tabela dbacademia.tbgrupo
+--
+-- Estrutura da tabela `tbgrupo`
+--
+
 CREATE TABLE IF NOT EXISTS `tbgrupo` (
-  `idgrupo` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `idgrupo` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `descricao` varchar(100) NOT NULL,
   PRIMARY KEY (`idgrupo`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- Copiando dados para a tabela dbacademia.tbgrupo: ~0 rows (aproximadamente)
+-- --------------------------------------------------------
 
--- Copiando estrutura para tabela dbacademia.tbitem_ficha
+--
+-- Estrutura da tabela `tbitem_ficha`
+--
+
 CREATE TABLE IF NOT EXISTS `tbitem_ficha` (
-  `iditem_ficha` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `idficha` int(11) unsigned DEFAULT NULL,
-  `idexercicio` int(11) unsigned DEFAULT NULL,
-  `serie` int(2) unsigned NOT NULL,
-  `repeticoes` int(2) unsigned NOT NULL,
+  `iditem_ficha` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `idficha` int(11) UNSIGNED DEFAULT NULL,
+  `idexercicio` int(11) UNSIGNED DEFAULT NULL,
+  `serie` int(2) UNSIGNED NOT NULL,
+  `repeticoes` int(2) UNSIGNED NOT NULL,
   `tipo_treino` char(1) NOT NULL,
   PRIMARY KEY (`iditem_ficha`),
   KEY `fk_ficha` (`idficha`),
-  KEY `fk_exercicio` (`idexercicio`),
-  CONSTRAINT `fk_exercicio` FOREIGN KEY (`idexercicio`) REFERENCES `tbexercicio` (`idexercicio`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `fk_ficha` FOREIGN KEY (`idficha`) REFERENCES `tbficha` (`idficha`) ON DELETE CASCADE ON UPDATE CASCADE
+  KEY `fk_exercicio` (`idexercicio`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- Copiando dados para a tabela dbacademia.tbitem_ficha: ~0 rows (aproximadamente)
+-- --------------------------------------------------------
 
--- Copiando estrutura para tabela dbacademia.tbusu
+--
+-- Estrutura da tabela `tbusu`
+--
+
 CREATE TABLE IF NOT EXISTS `tbusu` (
-  `idusu` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `idusu` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `email` varchar(100) NOT NULL,
   `senha` varchar(100) NOT NULL,
-  `cargo` char(1) NOT NULL DEFAULT 'G' COMMENT 'G = Gerente(admin), A = Aluno, E = Educador Físico',
+  `cargo` varchar(15) NOT NULL,
   PRIMARY KEY (`idusu`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- Copiando dados para a tabela dbacademia.tbusu: ~3 rows (aproximadamente)
-INSERT INTO `tbusu` (`idusu`, `email`, `senha`, `cargo`) VALUES
-	(1, 'admin@admin.com', '123', 'G'),
-	(2, 'aluno@aluno.com', '456', 'A'),
-	(3, 'educador@edu.com', '123', 'E');
+--
+-- Restrições para despejos de tabelas
+--
 
-/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+--
+-- Limitadores para a tabela `tbaluno`
+--
+ALTER TABLE `tbaluno`
+  ADD CONSTRAINT `fk_usu` FOREIGN KEY (`idusu`) REFERENCES `tbusu` (`idusu`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Limitadores para a tabela `tbavaliacao`
+--
+ALTER TABLE `tbavaliacao`
+  ADD CONSTRAINT `fk_aluno` FOREIGN KEY (`idaluno`) REFERENCES `tbaluno` (`idaluno`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_educador` FOREIGN KEY (`ideducador`) REFERENCES `tbeducador` (`ideducador`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_grupo` FOREIGN KEY (`idgrupo`) REFERENCES `tbgrupo` (`idgrupo`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Limitadores para a tabela `tbeducador`
+--
+ALTER TABLE `tbeducador`
+  ADD CONSTRAINT `fk_usu2` FOREIGN KEY (`idusu`) REFERENCES `tbusu` (`idusu`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Limitadores para a tabela `tbexercicio`
+--
+ALTER TABLE `tbexercicio`
+  ADD CONSTRAINT `fk_grupo2` FOREIGN KEY (`idgrupo`) REFERENCES `tbgrupo` (`idgrupo`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Limitadores para a tabela `tbficha`
+--
+ALTER TABLE `tbficha`
+  ADD CONSTRAINT `fk_ava` FOREIGN KEY (`idava`) REFERENCES `tbavaliacao` (`idava`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Limitadores para a tabela `tbitem_ficha`
+--
+ALTER TABLE `tbitem_ficha`
+  ADD CONSTRAINT `fk_exercicio` FOREIGN KEY (`idexercicio`) REFERENCES `tbexercicio` (`idexercicio`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_ficha` FOREIGN KEY (`idficha`) REFERENCES `tbficha` (`idficha`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
