92debf8f242a2462d76ae34369964febeb719996
Jhonatas56 <118845977+Jhonatas56@users.noreply.github.com>
2023-05-23 11:33:29 -0300

    Arquivos do Projeto
    Arquivos do aluno criados

--- aluno/minhaava.php
+++ aluno/minhaava.php
@@ -9,34 +9,34 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Monster Machine</title>
     <!-- plugins:css -->
-    <link rel="stylesheet" href="assets/vendors/fontawesome-free/css/fontawesome.min.css"> 
-    <link rel="stylesheet" href="assets/vendors/fontawesome-free/css/all.min.css">
-    <link rel="stylesheet" href="assets/vendors/css/vendor.bundle.base.css">
+    <link rel="stylesheet" href="../assets/vendors/fontawesome-free/css/fontawesome.min.css"> 
+    <link rel="stylesheet" href="../assets/vendors/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="../assets/vendors/css/vendor.bundle.base.css">
     <!-- endinject -->
     <!-- Plugin css for this page -->
-    <link rel="stylesheet" href="assets/vendors/jvectormap/jquery-jvectormap.css">
-    <link rel="stylesheet" href="assets/vendors/flag-icon-css/css/flag-icon.min.css">
-    <link rel="stylesheet" href="assets/vendors/owl-carousel-2/owl.carousel.min.css">
-    <link rel="stylesheet" href="assets/vendors/owl-carousel-2/owl.theme.default.min.css">
+    <link rel="stylesheet" href="../assets/vendors/jvectormap/jquery-jvectormap.css">
+    <link rel="stylesheet" href="../assets/vendors/flag-icon-css/css/flag-icon.min.css">
+    <link rel="stylesheet" href="../assets/vendors/owl-carousel-2/owl.carousel.min.css">
+    <link rel="stylesheet" href="../assets/vendors/owl-carousel-2/owl.theme.default.min.css">
     <!-- End plugin css for this page -->
     <!-- inject:css -->
     <!-- endinject -->
     <!-- Layout styles -->
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="../assets/css/style.css">
     <!-- End layout styles -->
-    <link rel="shortcut icon" href="assets/images/favicon.png" />
+    <link rel="shortcut icon" href="../assets/images/favicon.png" />
   </head>
   <body>
     <div class="container-scroller">
       <!-- partial:partials/_sidebar.html -->
       <nav class="sidebar sidebar-offcanvas" id="sidebar">
         <div class="sidebar-brand-wrapper d-none d-lg-flex align-items-center justify-content-center fixed-top">
-          <a class="sidebar-brand brand-logo" href="#"><img src="assets/images/logo.svg" alt="logo" /></a>
-          <a class="sidebar-brand brand-logo-mini" href="#"><img src="assets/images/logo-mini.svg" alt="logo" /></a>
+          <a class="sidebar-brand brand-logo" href="#"><img src="../assets/images/logo.svg" alt="logo" /></a>
+          <a class="sidebar-brand brand-logo-mini" href="#"><img src="../assets/images/logo-mini.svg" alt="logo" /></a>
         </div>
         <ul class="nav">
-          <li class="nav-item menu-items active">
-            <a class="nav-link" href="#">
+          <li class="nav-item menu-items">
+            <a class="nav-link" href="../aluno.php">
               <span class="menu-icon">
                 <i class="fa-solid fa-house"></i>
               </span>
@@ -44,15 +44,15 @@
             </a>
           </li>
           <li class="nav-item menu-items">
-            <a class="nav-link" href="aluno/minhaficha.php">
+            <a class="nav-link" href="minhaficha.php">
               <span class="menu-icon">
                 <i class="fa-solid fa-clipboard-list"></i>
               </span>
               <span class="menu-title">Minha Ficha</span>
             </a>
           </li>
-          <li class="nav-item menu-items">
-            <a class="nav-link" href="aluno/minhaava.php">
+          <li class="nav-item menu-items active">
+            <a class="nav-link" href="#">
               <span class="menu-icon">
                 <i class="fa-solid fa-ruler"></i>
               </span>
@@ -60,7 +60,7 @@
             </a>
           </li>
           <li class="nav-item menu-items">
-            <a class="nav-link" href="essenciais/sair.php">
+            <a class="nav-link" href="../essenciais/sair.php">
               <span class="menu-icon">
                 <i class="fa-solid fa-right-from-bracket"></i>
               </span>
@@ -102,18 +102,18 @@
       </div>
     </div>
     <!-- plugins:js -->
-    <script src="assets/vendors/js/vendor.bundle.base.js"></script>
+    <script src="../assets/vendors/js/vendor.bundle.base.js"></script>
     <!-- endinject -->
     <!-- End plugin js for this page -->
     <!-- inject:js -->
-    <script src="assets/js/off-canvas.js"></script>
-    <script src="assets/js/hoverable-collapse.js"></script>
-    <script src="assets/js/misc.js"></script>
-    <script src="assets/js/settings.js"></script>
-    <script src="assets/js/todolist.js"></script>
+    <script src="../assets/js/off-canvas.js"></script>
+    <script src="../assets/js/hoverable-collapse.js"></script>
+    <script src="../assets/js/misc.js"></script>
+    <script src="../assets/js/settings.js"></script>
+    <script src="../assets/js/todolist.js"></script>
     <!-- endinject -->
     <!-- Custom js for this page -->
-    <script src="assets/js/dashboard.js"></script>
+    <script src="../assets/js/dashboard.js"></script>
     <!-- End custom js for this page -->
   </body>
 </html>
