f316aa0418a9b06c0d5a6b66fcd262f16a5412ec
Everton Ferraz <eferraz194@gmail.com>
2023-06-26 21:43:40 -0300

    7

--- administrador/usuario/buscausu.php
+++ administrador/usuario/buscausu.php
@@ -8,7 +8,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Monster Machine</title>
+    <title>Corona</title>
     <!-- plugins:css -->
     <link rel="stylesheet" href="../../assets/vendors/fontawesome-free/css/fontawesome.min.css"> 
     <link rel="stylesheet" href="../../assets/vendors/fontawesome-free/css/all.min.css">
